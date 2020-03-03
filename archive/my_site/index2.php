e6627635734f33b57160093f42e055e8e7772430
chexare <chezcrew@gmail.com>
2020-03-03 00:15:09 -0500

    rearrange some stuff, testing a site
    rearrange some stuff, testing a site

---
+++ archive/my_site/index2.php
@@ -0,0 +1,7 @@
+<?php   
+$client = new MongoClient('localhost:27017/test');
+var_dump($client);
+echo "some";
+?>
+
+
--- my_site/index2.php
+++
@@ -1,7 +0,0 @@
-<?php   
-$client = new MongoClient('localhost:27017/test');
-var_dump($client);
-echo "some";
-?>
-
-
